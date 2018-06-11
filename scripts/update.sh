e4bff7d70204dcefcbbb8af20926264c1e735608
andresgnlez <andres.gnlez@gmail.com>
2018-06-11 22:25:19 +0200

    Adds some documentation

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,12 +4,14 @@
 sudo softwareupdate --install --all --restart
 
 # Brew formulas
-brew update
-brew outdated
-brew upgrade
+brew update # updates formulaes and Homebrew
+brew outdated # shows outdated installed formulas after updating them
+brew upgrade # Upgrades once formulaes are updated
+# removes old versions of a formulae
+# modificator shows what will be cleaned up
 brew cleanup -n
-brew prune
-brew doctor
+brew prune # removes old symlinks
+brew doctor # checks if everything is OK
 
 # Brew casks
-brew cask upgrade
+brew cask upgrade # updates apps installed through cask

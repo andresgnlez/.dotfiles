c1e3e6f46c8947c2cce802d9522da2fea0dc87a0
Andrés González Muñoz <andres.gnlez@gmail.com>
2019-02-01 18:29:12 +0100

    Removes deprecated brew prune and adds log info

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo '[INFO] Looking for system updates...'
+
 # System updates (restarts if required)
 sudo softwareupdate --list --all --install --restart
+
+echo '[INFO] Done wiht system updates.'
+
+echo '[INFO] Updating brew formulaes...'
 
 # Brew formulas
 brew update # updates formulaes and Homebrew
 brew outdated # shows outdated installed formulas after updating them
-brew upgrade # Upgrades once formulaes are updated
-# removes old versions of a formulae
-brew cleanup
-brew prune # removes old symlinks
+brew upgrade # upgrades once formulaes are updated
+brew cleanup # removes old versions of a formulae
 brew doctor # checks if everything is OK
+
+echo '[INFO] You are up to date! 🎉🎉'

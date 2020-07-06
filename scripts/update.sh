c3d15dd697717b540b25e5cc6e452dfea9aa2eba
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2020-07-06 10:43:46 +0200

    adds verbose option to bew outdated. Rewording.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Brew formulae
-brew update # updates formulaes and Homebrew
-brew outdated # shows outdated installed formulas after updating them
-brew upgrade # upgrades once formulaes are updated
+echo '[INFO] Updating brew formulae...'
+
+brew update # updates formulae and Homebrew
+brew outdated --verbose # shows detailed version information of outdated installed formulae after updating them
+brew upgrade # upgrades once formulae are updated
 brew cleanup # removes old versions of a formulae
 brew doctor # checks if everything is OK
-
 
 echo '[INFO] Looking for system updates...'
 
@@ -14,6 +15,5 @@ echo '[INFO] Looking for system updates...'
 sudo softwareupdate --list --all --install --restart
 
 echo '[INFO] Done with system updates.'
-echo '[INFO] Updating brew formulae...'
 
 echo '[INFO] You are up to date! 🎉🎉'
